ed766ec3d8b9a860a5734116647088c80afe1fe1
rendijsberkis <rendijsberkis7@gmail.com>
2025-02-15 21:35:30 +0200

    Salaboju resource collection

--- app/Http/Resources/Auth/UserResourceCollection.php
+++ app/Http/Resources/Auth/UserResourceCollection.php
@@ -4,15 +4,12 @@ declare(strict_types=1);
 
 namespace App\Http\Resources\Auth;
 
-use App\Http\Resources\Auth\UserResource;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
 class UserResourceCollection extends ResourceCollection
 {
     public function toArray($request): array
     {
-        return [
-            'users' => UserResource::collection($this->collection)
-        ];
+        return $this->collection->toArray();
     }
 }
